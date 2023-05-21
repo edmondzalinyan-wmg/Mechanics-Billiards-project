56b13652a406be264871471ae1b5f6505f5b44d5
Edmond <edmond.zalinyan@wmg.com>
2023-05-21 16:45:26 +0400

    implemented task1 flow

---
+++ src/Task1.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        Particle p = Particle.createRandom();
+        int n = 50;
+        List<Position> positionList = new ArrayList<>();
+        List<Position> reversePositionList = new ArrayList<>();
+        for (int i = 0; i < n; i++) { //iterate the path
+            p.moveTillReflection();
+            p.reflect();
+            positionList.add(p.getPosition());
+        }
+        p.moveTillReflection();
+//        do not reflect this time
+        p.reverse();
+        for (int i = 0; i < n; i++) { //iterate the reversed path
+            p.moveTillReflection();
+            p.reflect();
+            reversePositionList.add(p.getPosition());
+        }
+        for (int i = 0; i < positionList.size(); i++) {
+            System.out.println(positionList.get(i) + " vs " + reversePositionList.get(n - i - 1));
+            /*
+            Points on the reversed path almost coincide with the points of the original path.
+            The tiny difference is because of how Java approximates doubles and happens on ~10^-16 precision.
+            */
+        }
+
+    }
+}
